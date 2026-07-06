906a40bf9d3410662568aabcb66c9bf3e3b33649
AdyaKumarjha <adyakumar8434@gmail.com>
2026-07-06 07:56:16 +0530

    Added Vercel frontend URL to CORS

--- backend/app.py
+++ backend/app.py
@@ -16,10 +16,14 @@ app = FastAPI(
 # CORS Configuration
 # ----------------------------
 origins = [
+    # Local development
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
+
+    # Vercel Frontend
+    "https://loan-iq-chat-bot-p13f.vercel.app",
 ]
 
 app.add_middleware(
